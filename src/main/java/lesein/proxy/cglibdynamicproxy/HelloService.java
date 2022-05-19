f2d22e59785d627a95a8910fb2597ceb3638de34
wangjie <1105905347@qq.com>
2022-05-19 18:07:09 +0800

    cglib代理

---
+++ src/main/java/lesein/proxy/cglibdynamicproxy/HelloService.java
@@ -0,0 +1,23 @@
+package lesein.proxy.cglibdynamicproxy;
+
+/**
+ * @author WangJie
+ * @date 2022/5/19
+ */
+public class HelloService {
+
+    public HelloService(){
+
+    }
+
+    /**
+     * final修饰的方法不能被代理
+     */
+    final public void sayBye(){
+        System.out.println("sayBye");
+    }
+
+    public void sayHello(){
+        System.out.println("sayHello");
+    }
+}
