8ef395f2ed38f0790e95993bd4e742ee4031eedb
wangjie <1105905347@qq.com>
2023-04-17 17:29:51 +0800

    gateway实现校验解析token并传递给下游

--- gateway/src/main/java/com/lesein/gateway/gatewaytoken/service/GatewayTokenService.java
+++ gateway/src/main/java/com/lesein/gateway/gatewaytoken/service/GatewayTokenService.java
@@ -1,30 +1,22 @@
 package com.lesein.gateway.gatewaytoken.service;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import com.lesein.common.base.response.BaseResponse;
-import com.lesein.common.base.util.JacksonUtil;
 import com.lesein.common.base.util.OkHttpUtil;
 import com.lesein.gateway.gatewaytoken.request.AnalysisRequest;
 import com.lesein.gateway.gatewaytoken.response.AnalysisResponse;
-import io.netty.buffer.ByteBufAllocator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.filter.factory.rewrite.CachedBodyOutputMessage;
 import org.springframework.cloud.gateway.support.BodyInserterContext;
 import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DataBufferUtils;
-import org.springframework.core.io.buffer.NettyDataBufferFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.codec.HttpMessageReader;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -36,15 +28,10 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import javax.annotation.Resource;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.nio.CharBuffer;
-import java.nio.charset.StandardCharsets;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author WangJie
@@ -52,7 +39,7 @@ import java.util.concurrent.atomic.AtomicReference;
  */
 @Component
 public class GatewayTokenService implements GlobalFilter {
-    private static final List<HttpMessageReader<?>> messageReaders = HandlerStrategies.withDefaults().messageReaders();
+    private static final List<HttpMessageReader<?>> HTTP_MESSAGE_READERS = HandlerStrategies.withDefaults().messageReaders();
 
     /**
      * 排除不需要鉴权的url
@@ -78,7 +65,7 @@ public class GatewayTokenService implements GlobalFilter {
                 }
                 DataBuffer buffer = response.bufferFactory().wrap(bytes);
                 return response.writeWith(Flux.just(buffer));
-            }else {
+            } else {
                 //解析token
                 AnalysisRequest analysisRequest=new AnalysisRequest();
                 analysisRequest.setToken(token);
@@ -88,30 +75,43 @@ public class GatewayTokenService implements GlobalFilter {
                 });
                 AnalysisResponse data = baseResponse.getData();
                 //获取requestBody，并增加从token中解析的数据
-                ServerHttpRequest request = exchange.getRequest();
-                HttpHeaders httpHeaders = request.getHeaders();
-                if (httpHeaders.getContentLength() > 0) {
-                    return DataBufferUtils.join(exchange.getRequest().getBody()).flatMap(dataBuffer -> {
-                        byte[] bytes = new byte[dataBuffer.readableByteCount()];
-                        dataBuffer.read(bytes);
-                        String bodyString = new String(bytes, StandardCharsets.UTF_8);
-                        JSONObject jsonObject =  JSON.parseObject(bodyString);
-                        jsonObject.put("userId",data.getUserId());
-                        String newBodyString=jsonObject.toString();
+                ServerRequest serverRequest = ServerRequest.create(exchange, HTTP_MESSAGE_READERS);
+                Mono<String> modifiedBody = serverRequest.bodyToMono(String.class)
+                        .flatMap(body -> {
+                            JSONObject jsonObject = JSONObject.parseObject(body);
+                            jsonObject.put("userId",data.getUserId());
+                            return Mono.just(jsonObject.toJSONString());
+                        });
+                BodyInserter bodyInserter = BodyInserters.fromPublisher(modifiedBody, String.class);
+                HttpHeaders headers = new HttpHeaders();
+                headers.putAll(exchange.getRequest().getHeaders());
 
-                        exchange.getAttributes().put("POST_BODY", newBodyString);
-                        DataBufferUtils.release(dataBuffer);
-                        Flux<DataBuffer> cachedFlux = Flux.defer(() -> Mono.just(exchange.getResponse().bufferFactory().wrap(bytes)));
+                headers.remove(HttpHeaders.CONTENT_LENGTH);
 
-                        ServerHttpRequest mutatedRequest = new ServerHttpRequestDecorator(exchange.getRequest()) {
-                            @Override
-                            public Flux<DataBuffer> getBody() {
-                                return cachedFlux;
-                            }
-                        };
-                        return chain.filter(exchange.mutate().request(mutatedRequest).build());
-                    });
-                }
+                CachedBodyOutputMessage outputMessage = new CachedBodyOutputMessage(exchange, headers);
+                return bodyInserter.insert(outputMessage, new BodyInserterContext())
+                        .then(Mono.defer(() -> {
+                            ServerHttpRequestDecorator decorator = new ServerHttpRequestDecorator(
+                                    exchange.getRequest()) {
+                                @Override
+                                public HttpHeaders getHeaders() {
+                                    long contentLength = headers.getContentLength();
+                                    HttpHeaders httpHeaders = new HttpHeaders();
+                                    httpHeaders.putAll(super.getHeaders());
+                                    if (contentLength > 0) {
+                                        httpHeaders.setContentLength(contentLength);
+                                    } else {
+                                        httpHeaders.set(HttpHeaders.TRANSFER_ENCODING, "chunked");
+                                    }
+                                    return httpHeaders;
+                                }
+                                @Override
+                                public Flux<DataBuffer> getBody() {
+                                    return outputMessage.getBody();
+                                }
+                            };
+                            return chain.filter(exchange.mutate().request(decorator).build());
+                        }));
 
             }
         }
